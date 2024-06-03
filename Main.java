3ac864119efa718fded5dca75839cc3ea1db1bda
Suril Pokharel <surilpokharel3@gmail.com>
2024-06-03 15:49:16 +0545

    New File

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+        int age=30,temperature=50;
+        System.out.println(age);
+        System.out.println(temperature);
+        System.out.println("Hello World");
+
+        long num=123456789;
+        System.out.println(num);
+        
+        for(int i=0;i<=num;i++){
+            System.out.println(i);
+            
+        }
+    }
+}
